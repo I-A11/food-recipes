f0efa630459e2c5f8d73ebe8421a4454e891df95
I-A11 <d.islam1983@gmail.com>
2021-05-22 21:23:13 +1000

    clear searching input after search

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const searchForm = document.querySelector(".form");
 const searchResult = document.querySelector(".search-result");
 const container = document.querySelector("flex-container");
-const btn = document.querySelector(".btn");
+
 let searchQuery = "";
 const APP_ID = "e463ff2b";
 const APP_Key = "d6f9444b805f1e92f44628bbba0179b3";
 
-btn,
-  searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    searchQuery = e.target.querySelector("input").value;
-    fetchAPI();
-  });
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+  fetchAPI();
+
+  e.target.querySelector("input").value = "";
+});
 
 async function fetchAPI() {
   const baseURL = `https://api.edamam.com/search?q=${searchQuery}a&app_id=${APP_ID}&app_key=${APP_Key}&to=20`;
   const response = await fetch(baseURL);
   const data = await response.json();
   generateHTML(data.hits);
-  console.log(data);
+  // console.log(data);
 }
 const generateHTML = (results) => {
   let generatedHTML = "";
